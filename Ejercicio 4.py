040a53b0c3cf2cc12d9f458e41e6bac4d515d3eb
Garyjnm <garyn7674@gmail.com>
2025-05-05 22:06:24 -0600

    Avances del 4to ejercicio

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -5,3 +5,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df_movies = pd.read_csv("./Data/Rotten Tomatoes Movies.csv")
+
+df_movies_copy = df_movies.copy()
+
+df_genres_exploded = df_movies_copy.assign(
+genre=df_movies_copy['genre'].str.split(',')
+).explode('genre')
